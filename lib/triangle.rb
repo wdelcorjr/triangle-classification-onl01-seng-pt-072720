0a3ad04cb76171e9a760a9ab1495a3b2ab66ad26
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-08-21 13:47:59 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  def kind
+    if @a + @b <= @c || @c + @b <= @a || @a + @c <= @b 
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif @a != @b && @a != @c && @b != @c
+      :scalene
+    else
+      :isosceles
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "That ain't no triangle!"
+    end
+  end  
 end
